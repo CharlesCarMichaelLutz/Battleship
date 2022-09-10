035148b0fce8b593dc6388b6b38f54af1d82d405
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-09-10 14:36:06 +0800

    ship assignment

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿
 namespace Battleship
 {
-    class Program
-    {      
+    internal class Program
+    {
         private static int row = 10;
         private static int column = 10;
 
@@ -11,35 +11,36 @@ namespace Battleship
         private static bool gameOver = false;
         static void Main(string[] args)
         {
-            //AddShip();
+            PopulateMap();
+            //user greeting
+            GreetUser();
+
             while (!gameOver)
             {
                 DrawMap();
-                Console.ReadLine();
 
-            }                      
-                    //hardcoded the ship location 
-                    gameBoard[0,5] = 0;
-                    gameBoard[0,6] = 0;
-                    gameBoard[0,7] = 0;
-                    gameBoard[0,8] = 0;
-                    gameBoard[0,9] = 0;
-                    Console.ReadLine();
+                Console.WriteLine($"X: 1 - {row}");
+                Console.WriteLine($"Y: 1 - {column}\n");
+                Console.WriteLine("Enter guess (X,Y): ");
+                string guess = Console.ReadLine();
 
-            //generate ship and place on board
-            Console.ReadLine();
+                try
+                {
+                    int xPos = Convert.ToInt32(guess.Split(',')[0]) - 1;
+                    int yPos = Convert.ToInt32(guess.Split(',')[1]) - 1;
 
-            //user greeting
-            GreetUser();
-           
-            //User input and gameplay logic
-            Console.WriteLine("\nEnter guess: ");
-            int guess = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("coolness");
-            Console.ReadLine();
+                    if (xPos > row - 1 || yPos > column - 1)
+                    {
+                        message = "You are off the board, try again!";
+                    }
+                    CheckGuess(xPos, yPos);
+                }
+                catch
+                {
+                    message = "Unable to process coordinates";
+                }
+            }
         }
-
         private static void GreetUser()
         {
             Console.WriteLine("Welcome to Battle! press Enter ");
@@ -49,94 +50,134 @@ namespace Battleship
             string username = Console.ReadLine();
             Console.WriteLine($"\nLet's begin {username}!");
         }
-        //private static void checkHit(int x, int y)
-        //{
-        //    if (gameBoard[x,y] == Square.Ship)
-        //    {
-        //        gameBoard[x,y] = Square.Hit;
-        //        Console.WriteLine("It's a hit!!!");
-        //    }
-        //    else if (gameBoard[x,y] == Square.Miss)
-        //    {
-        //        Console.WriteLine("you guessed this before");
-        //    }
-        //    else if(gameBoard[x,y] == Square.Water)
-        //    {
-        //        gameBoard[x,y] = Square.Miss;
-        //    }
-        //    return gameBoard[x,y];
-        //}
+        private static void CheckGuess(int x, int y)
+        {
+            if (gameBoard[y, x] == 3)
+            {
+                gameBoard[y, x] = 1;
 
-        //clear the game board
-            //Console.Clear();
+                if (CheckWin())
+                {
+                    message = "You Won!!!";
+                    DrawMap();
 
-        //private static void AddShip()
-        //{
-        //    Random random = new Random();
-        //    string direction;
+                    Console.Write("Do you want to play again? (y/n): ");
+                    string answer = Console.ReadLine();
+                    if (answer.ToLower() == "y")
+                    {
+                        PopulateMap();
+                        message = "";
+                        return;
+                    }
+                    else
+                    {
+                        gameOver = true;
+                        return;
+                    }
+                }
+                message = "It's a Hit!";
+            }
+            else
+            {
+                message = "You Missed";
+                gameBoard[y, x] = 2;
+            }
+        }
 
-        //    int x = random.Next();
-        //    int y = random.Next();
+        private static bool CheckWin()
+        {
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < row; j++)
+                {
+                    if (gameBoard[i, j] == 3)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+        private static void PopulateMap()
+        {
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < column; j++)
+                {
+                    gameBoard[i, j] = 0;
+                }
+            }
+            AddShip();
+        }
+        private static void AddShip()
+        {
+            Random random = new Random();
+            string direction;
 
-        //    if (x < 5)
-        //    {
-        //        direction = "right";
-        //    }
-        //    else if (x > 5)
-        //    {
-        //        direction = "left";
-        //    }
-        //    else
-        //    {
-        //        direction = "left,right";
-        //    }
+            int x = random.Next(0,row -1);
+            int y = random.Next(0, column -1);
 
-        //    if (y < 5)
-        //    {
-        //        direction = "down";
-        //    }
-        //    else if (y > 5)
-        //    {
-        //        direction = "up";
-        //    }
-        //    else
-        //    {
-        //        direction = "up,down";
-        //    }
+            if (x < 5)
+            {
+                direction = "right";
+            }
+            else if (x > 5)
+            {
+                direction = "left";
+            }
+            else
+            {
+                direction = "left,right";
+            }
 
-        //    direction = direction.Split(',')[random.Next(0, direction.Split(',').Length];
+            if (y < 5)
+            {
+                direction += "down";
+            }
+            else if (y > 5)
+            {
+                direction += "up";
+            }
+            else
+            {
+                direction += "up,down";
+            }
 
-        //    if (direction == "up")
-        //    {
-        //        for(int i = y; i < y - 5; i--)
-        //        {
-        //            map[i, x] = 3;
-        //        }
-        //    }else if (direction == "down")
-        //    {
-        //        for(int i = y; i < y + 5; i++)
-        //        {
-        //            map[i, x] = 3;
-        //        }                   
-        //    }else if (direction == "left")
-        //    {
-        //        for (int i = x; i < x - 5; i--)
-        //        {
-        //            map[y, i] = 3;
-        //        }
-        //    }else
-        //    {
-        //        for (int i = x; i < x + 5; i++)
-        //        {
-        //            map[y, i] = 3;
-        //        }
-        //    }
+            direction = direction.Split(',')[random.Next(0, direction.Split(',').Length)];
 
+            if (direction == "up")
+            {
+                for (int i = y; i < y - 5; i--)
+                {
+                    gameBoard[i, x] = 3;
+                }
+            }
+            else if (direction == "down")
+            {
+                for (int i = y; i < y + 5; i++)
+                {
+                    gameBoard[i, x] = 3;
+                }
+            }
+            else if (direction == "left")
+            {
+                for (int i = x; i < x - 5; i--)
+                {
+                    gameBoard[y, i] = 3;
+                }
+            }
+            else
+            {
+                for (int i = x; i < x + 5; i++)
+                {
+                    gameBoard[y, i] = 3;
+                }
+            }
+        }
         private static void DrawMap()
         {
             Console.Clear();
             DrawLogo();
-
             Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.Write("+");
             for (int j = 0; j < row; j++)
@@ -144,7 +185,6 @@ namespace Battleship
                 Console.Write("-");
             }
             Console.Write("+\n");
-
             for (int i = 0; i < column; i++)
             {
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
@@ -174,18 +214,15 @@ namespace Battleship
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
                 Console.Write("|\n");
             }
-
             Console.Write("+");
             for (int j = 0; j < row; j++)
             {
                 Console.Write("-");
             }
             Console.Write("+\n");
-
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(message);
         }
-
         private static void DrawLogo()
         {
             Console.Write("\n");
@@ -198,7 +235,7 @@ namespace Battleship
             Console.Write("\n");
         }
     }
-    }
+}
 
 
 
