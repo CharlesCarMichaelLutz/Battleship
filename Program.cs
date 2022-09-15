ee16a1c69345ee021b248dabcf9e7b651580c3e5
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-09-15 18:55:05 +0800

    sink or swim

--- Program.cs
+++ Program.cs
@@ -1,10 +1,8 @@
 ﻿
 namespace ConsoleBattle
-{
-    internal class Program
+{ 
+    public class Program
     {
-        private static bool finished = false;
-        string message;
         public enum Square
         {
             Water,
@@ -12,16 +10,39 @@ namespace ConsoleBattle
             Ship,
             Hit,
         }
+
+        private static bool finished = false;
+        private static string message;
         public static void Main(string[] args)
-        {            
+        {
             Square[,] TheBoard = new Square[10, 10];
 
+            Console.WriteLine(TheBoard[4, 6]);
             while (!finished)
             {
                 Welcome();
-                UserInput();
-                //CheckGuess();
 
+                Console.WriteLine("X: 0 - 9");
+                Console.WriteLine("Y: 0 - 9");
+                Console.WriteLine("Enter your guess: X, Y");
+                string guess = Console.ReadLine();
+
+                try
+                {
+                    int xPos = Convert.ToInt32(guess.Split(',')[0]) - 1;
+                    int yPos = Convert.ToInt32(guess.Split(',')[1]) - 1;
+
+                    if (xPos > 9 || yPos > 9)
+                    {
+                        message = "You are off the board, try again!";
+                    }
+                    CheckGuess(xPos, yPos);
+                }
+                catch
+                {
+                    message = "Unable to process coordinates";
+                }
+             
                 finished = true;
             }
         }
@@ -31,41 +52,32 @@ namespace ConsoleBattle
             //Console.ReadLine();
             Console.WriteLine("Enter username: ");
             string username = Console.ReadLine();
-            Console.WriteLine($"\nLet's begin {username}!");
+            Console.WriteLine($"\nLet's begin {username} press Enter!");
+            Console.ReadLine(); 
         }
-        private static void UserInput()
+        public static void CheckGuess(int x, int y)
         {
-            Console.WriteLine("X: 0 - 9");
-            Console.WriteLine("Y: 0 - 9");
-            Console.WriteLine("Enter your guess: X, Y");
-            string guess = Console.ReadLine();
-            Console.WriteLine($"You guessed: {guess}");
-            Console.ReadLine();
+            if (TheBoard[x, y] == Square.Ship)
+            {
+                TheBoard[x, y] = Square.Hit;
+            }
+            else if (TheBoard[x, y] == Square.Water)
+            {
+                TheBoard[x, y] = Square.Miss;
+            }
+            else if (TheBoard[x, y] == Square.Miss)
+            {
+                message = "you already missed here";
+            }
+            else
+            {
+                message = "this spot was hit already";
+            }
         }
     }
 }
 
-//private static void CheckGuess()
-//{
-//    if (TheBoard[x, y] == Square.Ship)
-//    {
-//        TheBoard[x, y] = Square.Hit;
-//    }
-//    else if (TheBoard[x, y] == Square.Water)
-//    {
-//        TheBoard[x, y] = Square.Miss;
-//    }
-//    else if (TheBoard[x, y] == Square.Miss)
-//    {
-//        message = "You already missed here";
-//    }
-//    else
-//    {
-//        message = "This spot was hit already";
-//    }
-//}
 
-//    }
-//}
+
 
 
